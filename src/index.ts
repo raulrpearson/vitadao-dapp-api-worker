c7f18a0f76d94d0ae484933496a0b33aee4b4119
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-11-29 17:26:05 +0200

    Commit WIP

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,37 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  VARIABLE: string;
 }
 
+const baseUrl = "https://random-data-api.com/api/v2";
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const { pathname } = new URL(request.url);
+    let response = await fetch(baseUrl + pathname, {
+      cf: {
+        // Cloudflare cache 5 minutes
+        cacheTtl: 300,
+        cacheEverything: true,
+      },
+    });
+
+    // Browser cache 5 minutes. Clone response to mutate.
+    response = new Response(response.body, response);
+    response.headers.set("Cache-Control", "max-age=300");
+    return response;
+  },
 };
