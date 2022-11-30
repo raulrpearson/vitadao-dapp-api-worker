9edb9d0adba8fe52d045973f370b310d4dba59bd
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-12-01 00:00:25 +0200

    Add quick index and random beer cache test

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,41 @@ export interface Env {
   TRANSPOSE_KEY: string;
 }
 
+const index = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>VitaDAO workers API</title>
+  </head>
+  <body style="max-width: 600px">
+    <h1>VitaDAO workers API</h1>
+    <p>Choose and endpoint</p>
+    <ul>
+      <li>
+        <a href="/history">history</a>: a timeseries of the total USD balance of
+        vitadao.eth's multisig. One data point per day.
+      </li>
+      <li>
+        <a href="/stats">stats</a>: circulating supply (currently total supply)
+        of VITA and its market cap.
+      </li>
+      <li>
+        <a href="/tokens">tokens</a>: list of tokens currently in vitadao.eth
+        and some data about each token.
+      </li>
+      <li>
+        <a href="/random">random</a>: a random beer fetched from
+        random-data-api.com, cached for 60 seconds on Cloudflare. I.e. you
+        should get a new beer only every minute, not on every request. The
+        purpose of this endpoint is to test that the caching works (which it
+        does, across Cloudflare datacenters).
+      </li>
+    </ul>
+  </body>
+</html>`;
+
 export default {
   async fetch(
     request: Request,
@@ -13,16 +48,30 @@ export default {
   ): Promise<Response> {
     console.log("parse query");
     let query;
-    switch (new URL(request.url).pathname.slice(1)) {
-      case "history":
+    switch (new URL(request.url).pathname) {
+      case "/history":
         query = queries.history;
         break;
-      case "stats":
+      case "/stats":
         query = queries.stats;
         break;
-      case "tokens":
+      case "/tokens":
         query = queries.tokens;
         break;
+      case "/random": {
+        return fetch("https://random-data-api.com/api/v2/beers", {
+          cf: {
+            cacheTtl: 60,
+            cacheEverything: true,
+          },
+        });
+      }
+      default:
+        return new Response(index, {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        });
     }
 
     console.log("fetch");
